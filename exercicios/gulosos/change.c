6fee430dc5d0707c06a9cf850a6ef8dc59c51d3e
Vinícius Brait <lorimiervb@gmail.com>
2025-05-18 21:29:36 +0000

    refactor: mudança no problema do troco e dos selos

--- exercicios/gulosos/change.c
+++ exercicios/gulosos/change.c
@@ -23,30 +23,31 @@
 */
 #include <stdio.h>
 
-void define(int *values, int size, int *value_cents, const char *type) {
+void define(float value) {
+    int notes[] = {10000, 5000, 2000, 1000, 500, 200};
+    int coins[] = {100, 50, 25, 10, 5, 1};
+    int cents = (int)(value * 100 + 0.5);
+
+    int size = sizeof(notes) / sizeof(notes[0]);
+
+    printf("NOTAS:\n");
     for (int i = 0; i < size; i++) {
-        int qtd = *value_cents / values[i];
-        printf("%d %s(s) de R$ %.2f\n", qtd, type, values[i] / 100.0);
-        *value_cents %= values[i];
+        int qtd = cents / notes[i];
+        cents %= notes[i];
+        printf("%d nota(s) de R$ %.2f\n", qtd, notes[i] / 100.0);
+    }
+
+    printf("MOEDAS:\n");
+    for (int i = 0; i < size; i++) {
+        int qtd = cents / coins[i];
+        cents %= coins[i];
+        printf("%d moeda(s) de R$ %.2f\n", qtd, coins[i] / 100.0);
     }
 }
 
 int main() {
-    double value;
-    int value_cents;
-
-    int notes[] = {10000, 5000, 2000, 1000, 500, 200};
-    int coins[] = {100, 50, 25, 10, 5, 1};
-
-    scanf("%lf", &value);
-
-    value_cents = (int)(value * 100 + 0.5);
-
-    printf("NOTAS:\n");
-    define(notes, 6, &value_cents, "nota");
-
-    printf("MOEDAS:\n");
-    define(coins, 6, &value_cents, "moeda");
+    float value = 576.73;
+    define(value);
 
     return 0;
 }
--- exercicios/gulosos/localMail.c
+++ exercicios/gulosos/localMail.c
@@ -7,27 +7,26 @@
 #include <stdio.h>
 
 void findStamps(int tax) {
-    int qtdStamps5, qtdStamps3;
-    int find = 0;
-
-    for (qtdStamps5 = tax / 5; qtdStamps5 >= 0; qtdStamps5--) {
-        int remaining = tax - (qtdStamps5 * 5);
-
+    int qtdStamps5 = tax / 5;
+    int remaining;
+    
+    for (int i = 0; i < 3 && qtdStamps5 >= 0; i++) {
+        remaining = tax - (qtdStamps5 * 5);
+        
         if (remaining % 3 == 0) {
-            qtdStamps3 = remaining / 3;
-            find = 1;
+            int qtdStamps3 = remaining / 3;
             printf("%d selo(s) de 5 centavos e %d selo(s) de 3 centavos\n", qtdStamps5, qtdStamps3);
             return;
         }
+        qtdStamps5--;
     }
 
-    if (!find)
-        printf("Impossível encontrar uma solução para essa taxa.");
+    printf("Impossível encontrar uma solução para essa taxa.");
 }
 
 int main(){
     int tax = 11;
-    findStamps(11);
+    findStamps(tax);
 
     return 0;
 }
